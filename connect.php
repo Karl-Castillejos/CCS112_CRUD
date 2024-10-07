91911ee30e7123b6f363a445c945ddcc60a2603a
renzcpgcn <capagcuanrenz13@gmail.com>
2024-10-07 09:12:21 +0800

    bg

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost:3308", "root", "", "todo_db");
+$con = mysqli_connect("localhost", "root", "", "todo_db");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
